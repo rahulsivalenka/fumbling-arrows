1e4fd3d88587407c15ab796d5253233c3140fefa
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2019-06-16 05:22:25 +0530

    Updates

---
+++ precache-manifest.36949d2426ca332a67c6f3bb777a19b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c707291546ca8f69a0b74c8e03f5ec3d",
+    "url": "/fumbling-arrows/index.html"
+  },
+  {
+    "revision": "f8c9d55d9b8f3cdfca7f",
+    "url": "/fumbling-arrows/static/css/main.9f9e8063.chunk.css"
+  },
+  {
+    "revision": "6bea2a1750d5e71fa6f7",
+    "url": "/fumbling-arrows/static/js/2.91120321.chunk.js"
+  },
+  {
+    "revision": "f8c9d55d9b8f3cdfca7f",
+    "url": "/fumbling-arrows/static/js/main.2a00dd4e.chunk.js"
+  },
+  {
+    "revision": "2d3ba121f4c93d9a2720",
+    "url": "/fumbling-arrows/static/js/runtime~main.16825812.js"
+  }
+]);
