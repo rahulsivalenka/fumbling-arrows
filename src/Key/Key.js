336a11e0c2cdf9c9144b8b93e9991269829f3776
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2019-07-06 07:21:33 +0530

    Gamified Keys

--- src/Key/Key.js
+++ src/Key/Key.js
@@ -4,11 +4,9 @@ import './Key.css';
 function Key(props) {
   return (
     <div className="Key">
-      {/* <div className="Key-base">
-        <div className="Key-top">^</div>
-      </div> */}
-      {/* ^ */}
-      <span className={`Key-arrow Key-${props.direction}`}></span>
+      <div className="Key-inner">
+        <div className={`Key-arrow Key-${props.direction}`}>^</div>
+      </div>
     </div>
   );
 }
